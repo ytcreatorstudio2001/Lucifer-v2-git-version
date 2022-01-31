8ce5507d73141a290bf3105500a0cc5e3891ab64
Ytstudio <91581305+ytcreatorstudio2001@users.noreply.github.com>
2022-01-31 16:05:08 +0530

    Update corona.py

--- plugins/admemes/corona.py
+++ plugins/admemes/corona.py
@@ -7,7 +7,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 API = "https://api.sumanjay.cf/covid/?country="
 
-BUTTONS = InlineKeyboardMarkup([[InlineKeyboardButton('⚙ Join our Channel ⚙', url='https://t.me/team_annaben')]])
+BUTTONS = InlineKeyboardMarkup([[InlineKeyboardButton('⚙ Join our Channel ⚙', url='https://t.me/MovieHub_2k22')]])
 
 @Client.on_message(filters.command("covid"))
 async def reply_info(client, message):
