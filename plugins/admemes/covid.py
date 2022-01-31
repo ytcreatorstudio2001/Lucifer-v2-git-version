2b7d02cb999d1b7e2f287818e789c427fb48088e
Ytstudio <91581305+ytcreatorstudio2001@users.noreply.github.com>
2022-01-31 16:04:02 +0530

    Update covid.py

--- plugins/admemes/covid.py
+++ plugins/admemes/covid.py
@@ -6,7 +6,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 API = "https://api.sumanjay.cf/covid/?country="
 
-BUTTONS = InlineKeyboardMarkup([[InlineKeyboardButton('ᴜᴘᴅᴀᴛᴇs', url='https://t.me/Annaben_support')]])
+BUTTONS = InlineKeyboardMarkup([[InlineKeyboardButton('ᴜᴘᴅᴀᴛᴇs', url='https://t.me/MovieHub_2k22')]])
 
 
 @Client.on_message(filters.command("covid"))
@@ -46,7 +46,7 @@ Latitude : `{latitude}`
 Longitude : `{longitude}`
 Recovered : `{recovered}`
 
-Made by @lallu_tg ❤️"""
+Made by ƬΣΛM ᄂЦᄃIFΣЯ V2 ❤️"""
         return covid_info
     except Exception as error:
         return error
